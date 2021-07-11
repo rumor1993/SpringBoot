48b1668c903ca27e03b955d7bed7b155cf3c9abf
rumor1993 <rumor1993@naver.com>
2021-07-12 02:41:33 +0900

    Spring Boot TestCode

---
+++ src/test/java/com/example/demo/service/MemberServiceTest.java
@@ -0,0 +1,25 @@
+package com.example.demo.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    @Test
+    void 회원가입() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void 전체조회() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
